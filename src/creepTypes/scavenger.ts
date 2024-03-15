c2eea6bed83fa7299fb584238a44c1e58dfff034
Adriano Cola <adriano@ensemble.com.br>
2024-03-14 22:36:19 -0300

    Added scavenger

---
+++ src/creepTypes/scavenger.ts
@@ -0,0 +1,63 @@
+import { pickup, transfer, withdraw } from 'utils/creep';
+import { getMainResourceHolder } from 'utils/room';
+
+const transferAllResources = (creep: Creep, mainResourceHolder: StructureContainer | StructureStorage) => {
+  const resources = Object.keys(creep.store);
+  for (const resource of resources) {
+    transfer(creep, mainResourceHolder, resource as ResourceConstant);
+    return true;
+  }
+
+  return false;
+};
+
+const scavengerCreepType: CreepType = {
+  name: CREEP_TYPE.SCAVENGER,
+  run(creep) {
+    const mainResourceHolder = getMainResourceHolder(creep.room);
+    if (!mainResourceHolder) return;
+
+    // if already full or near to mainResourceHolder, transfer all resources to mainResourceHolder
+    if (
+      creep.store.getFreeCapacity() === 0 ||
+      (creep.store.getFreeCapacity() !== 0 && creep.pos.isNearTo(mainResourceHolder))
+    ) {
+      if (transferAllResources(creep, mainResourceHolder)) return;
+    }
+
+    const tombstones = creep.room.find(FIND_TOMBSTONES);
+    for (const tombstone of tombstones) {
+      const resources = Object.keys(tombstone.store);
+      for (const resource of resources) {
+        if (resource !== RESOURCE_ENERGY && tombstone.store[resource as ResourceConstant] > 0) {
+          withdraw(creep, tombstone, resource as ResourceConstant);
+          return;
+        }
+      }
+      if (tombstone.store[RESOURCE_ENERGY] > 0) {
+        withdraw(creep, tombstone, RESOURCE_ENERGY);
+        return;
+      }
+    }
+
+    const droppedResources = creep.room.find(FIND_DROPPED_RESOURCES);
+    for (const resource of droppedResources) {
+      if (resource.resourceType !== RESOURCE_ENERGY) {
+        pickup(creep, resource);
+        return;
+      }
+    }
+    const resourceEnergy = droppedResources[0];
+    if (resourceEnergy) {
+      pickup(creep, resourceEnergy);
+      return;
+    }
+
+    // if there is no resources to pickup, transfer all resources to mainResourceHolder
+    if (creep.store.getUsedCapacity() > 0) {
+      transferAllResources(creep, mainResourceHolder);
+    }
+  },
+};
+
+export default scavengerCreepType;
--- src/roomSystems/defense.ts
+++ src/roomSystems/defense.ts
@@ -1,6 +1,39 @@
 import spawnSystem from './spawn';
 import { countBodyParts } from 'utils/creepBody';
 import workerDefender from 'creepTypes/defender';
+import scavengeCreepType from 'creepTypes/scavenger';
+import workerHarvesterWalker from 'creepTypes/harvesterWalker';
+
+const SCAVENGE_ENERGY_THRESHOLD = 2000;
+
+const scavengeResources = (room: Room) => {
+  let energy: number = 0;
+  const tombstones = room.find(FIND_TOMBSTONES);
+
+  const goodToScavenge = tombstones.some(tombstone => {
+    energy += tombstone.store.getUsedCapacity(RESOURCE_ENERGY) ?? 0;
+    const haveResourceOtherThanEnergy = Object.keys(tombstone.store).some(r => r !== RESOURCE_ENERGY);
+    return haveResourceOtherThanEnergy || energy > SCAVENGE_ENERGY_THRESHOLD;
+  });
+
+  if (!goodToScavenge) {
+    spawnSystem.removeSpawn(room, scavengeCreepType.name);
+    return;
+  }
+
+  spawnSystem.spawn(room, scavengeCreepType.name, workerHarvesterWalker.name, 1, 10, {
+    sectionParts: {
+      [CARRY]: 1,
+      [MOVE]: 1,
+    },
+    maxSections: 10,
+    memory: {
+      type: scavengeCreepType.name,
+      demandId: scavengeCreepType.name,
+      roomName: room.name,
+    },
+  });
+};
 
 const systemDefense: RoomSystem = {
   interval: TICKS.TICK_2,
@@ -26,7 +59,12 @@ const systemDefense: RoomSystem = {
       //   }
       // }
     } else {
-      room.memory.defense = { queue: [] };
+      // if there was a queue, it means that some enemy might have died and have some resources to scavenge
+      if (room.memory.defense?.queue.length) {
+        scavengeResources(room);
+      }
+
+      delete room.memory.defense;
     }
 
     if (room.memory.scan?.features?.[ROOM_FEATURE.HAVE_TOWERS]) return;
